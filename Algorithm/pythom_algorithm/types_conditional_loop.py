ad1f69d443580ea6e2949cda219eafcb465a26fd
baggy102 <rlgh0806@gmail.com>
2023-07-08 18:36:27 +0900

    algorithm_pythin: 반복문

--- Algorithm/pythom_algorithm/types_conditional_loop.py
+++ Algorithm/pythom_algorithm/types_conditional_loop.py
@@ -133,3 +133,130 @@ if x > 0 and x < 20:
 x = 15
 if 0 < x < 20:
 	print("x는 0 이상 20 미만의 수 입니다.")
+
+
+# 반복문
+'''
+특정한 소스코드를 반복적으로 실행하고자 할 때 사용하는 문법이다.
+파이썬에서는 While문과 for문이 있다.
+ - 코딩 테스트 시 for문을 사용하는것이 더 간결한 경우가 많다.
+'''
+
+# 1부터 9까지 모든 정수의 합 구하기 (while)
+i = 1 
+result = 0
+
+# i 가 9보다 작거나 같을 때 아래 코드를 반복적으로 실행
+while i <= 9:
+	result += 1
+	i += 1
+
+print(result)
+
+# 1부터 9까지 홀수의 합 구하기
+
+i = 1
+result = 0
+
+while i <= 9:
+	if i % 2 == 1:
+		result += 1
+	i += 1
+
+print(result)
+
+
+# 반복문에서의 무한 루프
+'''
+무한 루프란 끊임없이 반복되는 반복 구문을 의미한다.
+ - 코딩 테스트에서 무한 루프를 구현할 일은 거의 없으니 유의해야한다.
+ - 반복문을 작성한 뒤에는 항상 반복문을 탈출할 수 있는지 확인해야한다.
+
+
+x = 10
+
+while x > 5:
+	print(x)
+'''
+
+
+# 반복문 : for문
+'''
+반복문으로 for문을 이용할 수 있다.
+for문의 구조는 다음과 같으며, 특정한 변수를 이요해 'in' 뒤에 오는 데이터(리스트, 튜플 등)에 포함되어 있는 원소를 첫 번째 인덱스부터 차례대로 하나씩 방문한다.
+
+for 변수 in 리스트:
+	실행할 소스코드
+'''
+
+array = [9, 8, 7, 6, 5]
+
+for x in array:
+	print(x)
+
+'''
+for문에서 연속적인 값을 차례대로 순회할 때 range()를 주로 사용한다.
+ - 이때 range(시작 값, 끝 값 +1) 형태로 사용한다.
+ - 인자를 하나만 넣으면 자동으로 시작 값은 0이 된다.
+'''
+
+result = 0 
+
+# i는 1부터 9까지의 모든 값을 순회
+for i in range(1, 10):
+	result += 1
+
+print(result)
+
+
+# continue 키워드
+
+# 반복문에서 남은 코드의 실행을 건너뛰고, 다음 반복을 진행하고자 할 때 continue를 사용한다.
+
+# 1부터 9까지의 홀수의 합을 구하는 경우
+result = 0 
+
+for i in range(1, 10):
+	if i % 2 == 0:
+		continue
+	result += 1
+
+print(result)
+
+
+# break 키워드
+
+# 반복문을 즉시 탈출하고자 할 때 break를 사용한다.
+
+# 1부터 5까지의 정수를 차례대로 출력하고자 할 때
+
+i = 1
+
+while True:
+	print("현재 i의 값:", i)
+	if i == 5:
+		break
+	i += 1
+
+# Ex. 학생들의 합격 여부 판단 : 점수가 80점 넘으면 합격
+scores = [90, 85, 77, 65, 97]
+
+for i in range(5):
+	if scores[i] >= 80:
+		print(i + 1, "번 학생은 합격입니다.")
+
+# Ex. 특정 번호의 학생은 제외하기
+scores = [90, 85, 77, 65, 97]
+cheating_student_list = {2, 4}
+
+for i in range(5):
+	if i + 1 in cheating_student_list:
+		continue
+	if scores[i] >= 80:
+		print(i + 1, "번 학생은 합격입니다.")
+
+# 중첩된 반복문 : 구구단 예제
+for i in range(2, 10):
+	for j in range(1, 10):
+		print(i, "X", j, "=", i * j)
+	print()
