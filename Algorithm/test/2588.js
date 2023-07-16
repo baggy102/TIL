5d7faf929c58cd378e76c430e510edb2a3044e59
baggy102 <rlgh0806@gmail.com>
2023-07-16 18:03:20 +0900

    algorithm_test: 2588 곱셈 문제

--- Algorithm/test/2588.js
+++ Algorithm/test/2588.js
@@ -4,10 +4,15 @@ const input = require("fs").readFileSync("example.txt").toString().split("\n");
 
 // const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
 
-let A = parseInt(input[0]);
-let B = parseInt(input[1]);
+let [A, B] = input;
 
-console.log(A);
-console.log(B);
+let [B0, B1, B2] = B.split("");
 
-console.log(input[0].slice(0, 1));
+let sum1 = parseInt(A * B2);
+let sum2 = parseInt(A * B1);
+let sum3 = parseInt(A * B0);
+
+console.log(sum1);
+console.log(sum2);
+console.log(sum3);
+console.log(sum1 + 10 * sum2 + 100 * sum3);
