900e49ae8b392fae25e69bb07ffa141e72a33af9
baggy102 <rlgh0806@gmail.com>
2023-07-17 23:37:27 +0900

    algorithm_test: 사칙연산 final

---
+++ Algorithm/test/11382.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+const input = require("fs").readFileSync("example.txt").toString().split(" ");
+
+// const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+
+let A = parseInt(input[0]);
+let B = parseInt(input[1]);
+let C = parseInt(input[2]);
+
+console.log(A + B + C);
