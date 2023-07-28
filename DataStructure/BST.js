9d72e57319b7abbaa950cbbaf85a5bd9522a0eb4
baggy102 <rlgh0806@gmail.com>
2023-07-28 18:04:03 +0900

    Datastructure : BST

---
+++ DataStructure/BST.js
@@ -0,0 +1,74 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  isEmpty() {
+    return this.root === null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.isEmpty()) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(root, newNode) {
+    if (newNode.value < root.value) {
+      if (root.left === null) {
+        root.left = newNode;
+      } else {
+        this.insertNode(root.left, newNode);
+      }
+    } else {
+      if (root.right === null) {
+        root.right = newNode;
+      } else {
+        this.insertNode(root.right, newNode);
+      }
+    }
+  }
+
+  search(root, value) {
+    if (!root) {
+      return false;
+    } else {
+      if (root.value === value) {
+        return true;
+      } else if (value < root.value) {
+        return this.search(root.left, value);
+      } else {
+        return this.search(root.right, value);
+      }
+    }
+  }
+}
+
+const bst = new BinarySearchTree();
+console.log("Tree is empty?", bst.isEmpty());
+
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+
+console.log(bst.search(bst.root, 10));
+console.log(bst.search(bst.root, 5));
+console.log(bst.search(bst.root, 15));
+console.log(bst.search(bst.root, 20));
+
+// how to add new node in BST?
+
+// 1. new node that will be added to the tree(contain a value, two pointers pointing at null)
+// 2. tree is empty ~
+// 3. tree is not empty (tree contains value)
